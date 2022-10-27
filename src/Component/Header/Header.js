19edd4cfd062599f65896ce97807c03942d9a8e5
nafizwalide <nafizwalide06@gmail.com>
2022-10-27 19:07:05 +0600

    second push

--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -1,10 +1,17 @@
 import React from 'react';
-
+import './Header.css'
 const Header = () => {
     return (
-        <div>
-           
-        </div>
+        <nav className='header'>
+            <h1 className='header-name'>GOODFOOD</h1>
+            <div className="link-site">
+                <a href="/home">Home</a>
+                <a href="/food">Food</a>
+                <a href="/login">Log-in</a>
+                <a href="/about">About</a>
+            </div>
+        
+        </nav>
     );
 };
 
